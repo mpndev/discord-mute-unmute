061da8c4d5898c202c9bc11f5ba11baf1e4c0d9a
mpndev <martin.nikolov@ffwagency.com>
2021-08-05 00:58:34 +0300

    asd

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.once('ready', () => {
-	console.log('Ready!')
-});
-
 client.on('message', (message) => {
     const channel = message.channel
     const members = channel.members
     if (shouldMute(message.content)) {
         members.forEach(member => {
-            member.voice.setMute(true)
-            member.voice.setDeaf(true)
+            if (member.voice.channel) {
+                member.voice.setMute(true)
+                member.voice.setDeaf(true)
+            }
         });
-        message.channel.send('Shushhhhh is ON');
+        message.channel.send('All members are muted');
     } else if (shouldUnMute(message.content)) {
         members.forEach(member => {
-            member.voice.setMute(false)
-            member.voice.setDeaf(false)
+            if (member.voice.channel) {
+                member.voice.setMute(false)
+                member.voice.setDeaf(false)
+            }
         });
-        message.channel.send('Shushhhhh is OFF');
+        message.channel.send('All members are unmuted');
     }
 });
-
-client.login(process.env.DISCORD_BOT_TOKEN);
 
 const shouldMute = (messageContent) => ['/shutup', '/vaymoodu', '/muteall', 'm'].includes(messageContent)
 const shouldUnMute = (messageContent) => ['/speakup', '/vaythora', '/unmuteall', 'u'].includes(messageContent)
 
-
+client.login(process.env.DISCORD_BOT_TOKEN);
